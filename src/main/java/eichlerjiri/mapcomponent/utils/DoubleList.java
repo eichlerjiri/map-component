84f952c2631c03583ba8401816112e2d035a1858
Jiří Eichler <eichler.jirka@gmail.com>
2019-07-28 16:25:24 +0200

    typo

--- src/main/java/eichlerjiri/mapcomponent/utils/DoubleList.java
+++ src/main/java/eichlerjiri/mapcomponent/utils/DoubleList.java
@@ -1,11 +1,5 @@
 package eichlerjiri.mapcomponent.utils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
 public class DoubleList {
 
     public double[] data = new double[8];
